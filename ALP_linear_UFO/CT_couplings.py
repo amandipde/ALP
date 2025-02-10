7521a3f29576c4eebc3fbe9b73c3595569d51717
Amandip De <34458152+amandipde@users.noreply.github.com>
2025-02-10 19:19:43 +0530

    Add files via upload

---
+++ ALP_linear_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.26
+# Mathematica version: 11.0.1 for Linux x86 (64-bit) (September 21, 2016)
+# Date: Wed 20 Sep 2017 14:57:32
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
